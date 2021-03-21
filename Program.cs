f98bb51c8b1f6881c4555d9575e479a73469f139
Ereno <mamustafa@wp.pl>
2021-03-21 21:26:32 +0100

    + added first version of menu, multilanguage and CheckIsDuplicatesExist [work in progress]

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace BaseLinker_Products_Multitool
+{
+    class Program
+    {
+        static string MainMenu()
+        {
+            string menuInput;
+            Console.WriteLine("================ "+ Resources.Language.MainMenu + " ================");
+            Console.WriteLine("1. "+ Resources.Language.Menu1);
+            Console.WriteLine("2. "+ Resources.Language.Menu2);
+            Console.WriteLine("3. "+ Resources.Language.Menu3);
+            Console.WriteLine("");
+            Console.WriteLine("4. "+ Resources.Language.Exit);
+
+            menuInput = Console.ReadLine();
+            return menuInput;
+        }
+
+        static void MainActivity()
+        {
+            Console.Clear();
+            string menuInput;
+
+            while (true)
+            {
+                menuInput = MainMenu();
+                Console.Clear();
+                switch (menuInput)
+                {
+                    case "1":
+                        Workers.CheckIsDuplicatesExist();
+                        Console.Clear();
+                        break;
+                    case "2":
+                        Workers.DeleteDuplicates();
+                        Console.Clear();
+                        break;
+                    case "3":
+                        Workers.CopyProductsBetweenBaselinkerAccounts();
+                        Console.Clear();
+                        break;
+                    case "4":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine(Resources.Language.WrongMenuInput + Environment.NewLine);
+                        break;
+                }
+            }
+        }
+
+        static void Logo()
+        {
+            Console.Clear();
+            Console.WriteLine("██████╗  █████╗ ███████╗███████╗██╗     ██╗███╗   ██╗██╗  ██╗███████╗██████╗ ");
+            Console.WriteLine("██╔══██╗██╔══██╗██╔════╝██╔════╝██║     ██║████╗  ██║██║ ██╔╝██╔════╝██╔══██╗");
+            Console.WriteLine("██████╔╝███████║███████╗█████╗  ██║     ██║██╔██╗ ██║█████╔╝ █████╗  ██████╔╝");
+            Console.WriteLine("██╔══██╗██╔══██║╚════██║██╔══╝  ██║     ██║██║╚██╗██║██╔═██╗ ██╔══╝  ██╔══██╗");
+            Console.WriteLine("██████╔╝██║  ██║███████║███████╗███████╗██║██║ ╚████║██║  ██╗███████╗██║  ██║");
+            Console.WriteLine("╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝");
+            Console.WriteLine("     ██████╗ ██████╗  ██████╗ ██████╗ ██╗   ██╗ ██████╗████████╗███████╗");
+            Console.WriteLine("     ██╔══██╗██╔══██╗██╔═══██╗██╔══██╗██║   ██║██╔════╝╚══██╔══╝██╔════╝");
+            Console.WriteLine("     ██████╔╝██████╔╝██║   ██║██║  ██║██║   ██║██║        ██║   ███████╗");
+            Console.WriteLine("     ██╔═══╝ ██╔══██╗██║   ██║██║  ██║██║   ██║██║        ██║   ╚════██║");
+            Console.WriteLine("     ██║     ██║  ██║╚██████╔╝██████╔╝╚██████╔╝╚██████╗   ██║   ███████║");
+            Console.WriteLine("     ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝  ╚═════╝   ╚═╝   ╚══════╝");
+            Console.WriteLine("          ███╗   ███╗██╗   ██╗██╗  ████████╗██╗████████╗ ██████╗  ██████╗ ██╗");
+            Console.WriteLine("          ████╗ ████║██║   ██║██║  ╚══██╔══╝██║╚══██╔══╝██╔═══██╗██╔═══██╗██║     ");
+            Console.WriteLine("          ██╔████╔██║██║   ██║██║     ██║   ██║   ██║   ██║   ██║██║   ██║██║     ");
+            Console.WriteLine("          ██║╚██╔╝██║██║   ██║██║     ██║   ██║   ██║   ██║   ██║██║   ██║██║     ");
+            Console.WriteLine("          ██║ ╚═╝ ██║╚██████╔╝███████╗██║   ██║   ██║   ╚██████╔╝╚██████╔╝███████╗");
+            Console.WriteLine("          ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝");
+            Thread.Sleep(1500);
+            Console.Clear();
+        }
+
+        static void Main(string[] args)
+        {
+            string input;
+            bool languageMenu = true;
+            Logo();
+
+            Console.WriteLine("[EN] Select language | [PL] Wybierz język:");
+            Console.WriteLine("1. PL");
+            Console.WriteLine("2. EN");
+
+            while (languageMenu)
+            {
+                input = Console.ReadLine();
+                if (input == "1" || input == "2" || input == "PL" || input == "EN")
+                {
+                    if (input == "1" || input == "PL")
+                    {
+                        Thread.CurrentThread.CurrentUICulture = new CultureInfo("pl-PL");
+                        languageMenu = false;
+                    }
+                    else
+                    {
+                        Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+                        languageMenu = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("[EN] Type 1 or 2 for select language | [PL] Wpisz 1 lub 2 aby wybrać język");
+                }
+            }
+
+            MainActivity();
+        }
+    }
+}
---
+++ Workers.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLinker_Products_Multitool
+{
+    class Workers
+    {
+        private static string GetTokenAPI()
+        {
+            Console.WriteLine(Resources.Language.EnterAPIToken);
+            return Console.ReadLine();
+        }
+
+        private static string GetCategory()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(Resources.Language.EnterCategory);
+            return Console.ReadLine();
+        }
+
+        private static string GetCheckingKey()
+        {
+            string checkingKey;
+            Console.WriteLine("");
+            Console.WriteLine(Resources.Language.HowToCheckDuplicates);
+            Console.WriteLine("1. " + Resources.Language.HowToCheckDuplicates1 + " (sku)");
+            Console.WriteLine("2. " + Resources.Language.HowToCheckDuplicates2 + " (ean)");
+            Console.WriteLine("3. " + Resources.Language.HowToCheckDuplicates3 + " (name)");
+            checkingKey = Console.ReadLine();
+            switch (checkingKey)
+            {
+                case "1":
+                    checkingKey = "sku";
+                    break;
+                case "2":
+                    checkingKey = "ean";
+                    break;
+                case "3":
+                    checkingKey = "name";
+                    break;
+                default:
+                    checkingKey = "ERROR";
+                    break;
+            }
+
+            return checkingKey;
+        }
+
+        private static bool CheckIsEverythingCorrect()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(Resources.Language.IsEverythingCorrect);
+            Console.WriteLine("1. " + Resources.Language.Yes);
+            Console.WriteLine("2. " + Resources.Language.No);
+            string IsEverythingCorrect = Console.ReadLine();
+
+            switch (IsEverythingCorrect)
+            {
+                case "1":
+                    return true;
+                case "2":
+                    return false;
+                default:
+                    Console.WriteLine(Resources.Language.WrongMenuInput);
+                    Console.ReadKey();
+                    return false;
+            }
+        }
+
+
+
+        public static void CheckIsDuplicatesExist()
+        {
+            string tokenAPI = GetTokenAPI();
+            string category = GetCategory();
+            string checkingKey = GetCheckingKey();
+
+            if (checkingKey == "ERROR")
+            {
+                Console.WriteLine(Resources.Language.WrongMenuInput);
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine(Resources.Language.YourOptions+":");
+            Console.WriteLine(Resources.Language.YourOptionsAPI+": "+ tokenAPI);
+            Console.WriteLine(Resources.Language.YourOptionsCategory+": "+ category);
+            Console.WriteLine(Resources.Language.YourOptionsCheckKey+": "+ checkingKey);
+
+            bool IsEverythingCorrect = CheckIsEverythingCorrect();
+
+            if (!IsEverythingCorrect)
+                return;
+
+            
+
+            //TO DO: CHECKING DUPLICATES HERE
+        }
+
+        public static void DeleteDuplicates()
+        {
+
+        }
+
+        public static void CopyProductsBetweenBaselinkerAccounts()
+        {
+
+        }
+    }
+}
